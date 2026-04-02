d843b70a3f380d609134ab15959a659d558596a3
Samson Swaraj Quadros <76877116+SAM50NSQ@users.noreply.github.com>
2026-04-02 14:54:07 +0530

    Add: buy-and-sell-crypto - submission-5

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = prices[0]
+        res = 0
+        for i in range(len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            elif prices[i] - buy > res:
+                res = prices[i] - buy
+        return res
